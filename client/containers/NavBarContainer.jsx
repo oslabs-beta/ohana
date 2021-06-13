84e2bce42d09916cebe5ae5b8794e12b01204a19
Patrick Allen <patrick@loveforty.io>
2021-06-12 17:05:48 -0700

    frontend layout and navbar

--- client/containers/NavBarContainer.jsx
+++ client/containers/NavBarContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core';
 
 
 
@@ -6,7 +7,13 @@ const NavBar = () => {
 
 
   return (
+    <div id='navbar'>
     <h4>this is navbar</h4>
+    <Button>Spaces</Button>
+    <Button>vClusters</Button>
+    <Button>Admin</Button>
+    </div>
+
   )
 }
 
