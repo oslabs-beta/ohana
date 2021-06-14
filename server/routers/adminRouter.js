a5157b0b2de8ba2694193abd0ca8ccace172fc88
lawrencehan650 <lawrencehan3@gmail.com>
2021-06-14 15:33:47 -0700

    adds verification middleware for isAdmin

--- server/routers/adminRouter.js
+++ server/routers/adminRouter.js
@@ -22,4 +22,12 @@ router.post('/login',
   }
 )
 
+router.post('/verify',
+  adminController.verifyAdmin,
+  (req, res) => {
+    const { isAdmin } = res.locals;
+    res.status(200).json(isAdmin);
+  }
+)
+
 module.exports = router;
