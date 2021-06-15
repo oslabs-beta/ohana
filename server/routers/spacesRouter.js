12c953a4426fec90220e2c87935d9e67f12adf19
Andy Kahn <andy.d.kahn@gmail.com>
2021-06-15 12:08:43 -0700

    add logic to spacesModel

--- server/routers/spacesRouter.js
+++ server/routers/spacesRouter.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const spacesController = require('../controllers/spacesController');
+const spacesModel = require('../models/spacesModel');
+
 
 router.post('/', spacesController.addNamespace, (req, res) => {
+  res.status(200).send('posted to database');
+})
+
+// below route may be consolidated into '/' route
+router.post('/add', spacesController.addNamespace, spacesModel.addNamespace, (req, res) => {
   res.status(200).send('posted to database');
 })
 
