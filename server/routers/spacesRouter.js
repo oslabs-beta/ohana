60a6ec9028934f9b46167f772d3b971fb4c0394a
Patrick Allen <patrick@loveforty.io>
2021-06-16 23:18:16 -0700

    spaces and vcluster containers render tables

--- server/routers/spacesRouter.js
+++ server/routers/spacesRouter.js
@@ -9,6 +9,10 @@ spacesController.createNamespace, (req, res) => {
   res.status(200).send('posted to database');
 })
 
+router.get('/fetch', spacesController.fetchSpaces, (req, res) => {
+  res.status(200).json(res.locals.spaces);
+})
+
 // below route may be consolidated into '/' route
 // router.post('/add', spacesController.addNamespace, spacesModel.addNamespace, (req, res) => {
 //   res.status(200).send('posted to database');
