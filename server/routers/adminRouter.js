4dc93dd9c3965b338e4d687cbf20ad5ab3cd5607
mikat <mikataressatodd@gmail.com>
2021-06-24 11:49:53 -0700

    functionality to verfiy admin and display correct page

--- server/routers/adminRouter.js
+++ server/routers/adminRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
+
 // router.post('/create',
 //   adminController.bcryptPassword,
 //   adminController.addNewAdmin,
