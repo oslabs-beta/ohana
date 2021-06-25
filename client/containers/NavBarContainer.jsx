f7b0cd39006cb613f6918bb364bd8031f77f9736
lawrencehan650 <lawrencehan3@gmail.com>
2021-06-24 20:28:35 -0700

    renders navbar on login and admin bar if isAdmin

--- client/containers/NavBarContainer.jsx
+++ client/containers/NavBarContainer.jsx
@@ -1,25 +1,28 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, Link } from '@material-ui/core';
 import { Switch, Route } from 'react-router-dom';
 import SpacesContainer from './SpacesContainer.jsx'
 import { useHistory } from 'react-router-dom';
-export const LoginContext = React.createContext();
+import { AppContext } from '../components/AppContext'
+
 const NavBar = () => {
+  const { isAdmin } = useContext(AppContext)
   const [isLoggedIn, setLoggedIn] = useState(false);
-  const [isAdmin, setAdmin] = useState(false);
+  // const [isAdmin, setAdmin] = useState(false);
+  let adminButton = '';
+  if (isAdmin) adminButton = <Button className='navButtons' onClick={() => {history.push('/admin')}}>Admin</Button>
   let history = useHistory();
   // console.log('nav', isLoggedIn)
   // console.log('nav admin', isAdmin)
   // console.log('nav context', LoginContext);
   // setLoggedIn(useContext(LoginContext))
+
   return (
     <div id='navbar'>
       {/* <Button>Spaces</Button>
     <Button onClick={handleCluster}>vClusters</Button>
      */}
-    <Button className='navButtons' onClick={() => {history.push('/admin')}}>
-      Admin
-    </Button>
+    {adminButton}
     <Button className='navButtons' onClick={() => {history.push('/spaces')}}>
       Spaces
     </Button>
